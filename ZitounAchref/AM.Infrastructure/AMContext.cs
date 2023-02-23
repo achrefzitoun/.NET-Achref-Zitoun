9b5f4c0a3a88b29c7f60c30c7414fbe677788eee
Achref Zitoun <92675276+achrefzitoun@users.noreply.github.com>
2023-02-23 18:46:03 +0100

    Add files via upload

---
+++ ZitounAchref/AM.Infrastructure/AMContext.cs
@@ -0,0 +1,26 @@
+﻿using AM.ApplicationCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.Infrastructure
+{
+    public class AMContext : DbContext
+    {
+        public DbSet<Flight> Flights {get;set;}
+        public DbSet<Passenger> Passengers {get;set;}
+        public DbSet<Plane> Planes {get;set;}
+      //  public DbSet<Staff> Staff {get;set;}
+        public DbSet<Traveller> Travellers {get;set;}
+        public DbSet<Test2> test2s { get;set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source = (localDB)\MsSqlLocalDB; initial catalog = AchrefZitounNet; Integrated Security=true;");
+        }
+    }
+}
