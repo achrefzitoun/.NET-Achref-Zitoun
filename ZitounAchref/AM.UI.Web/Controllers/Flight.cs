ce1310d8b0e2e29f58194688f8854059fd243fda
Achref Zitoun <92675276+achrefzitoun@users.noreply.github.com>
2023-04-13 14:59:53 +0100

    Add files via upload

---
+++ ZitounAchref/AM.UI.Web/Controllers/Flight.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AM.ApplicationCore.Interfaces;
+using AM.ApplicationCore.Services;
+
+namespace AM.UI.Web.Controllers
+{
+    public class Flight : Controller
+    {
+
+        IServiceFlight serviceFlight;
+
+        public Flight(IServiceFlight serviceFlight)
+        {
+            this.serviceFlight = serviceFlight;
+        }
+        // GET: Flight
+        public ActionResult Index()
+        {
+            var flights = serviceFlight.GetAll();
+            return View(flights);
+        }
+
+        // GET: Flight/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Flight/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Flight/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Flight/Edit/5
+        public ActionResult Edit(int id)
+        {
+            
+            return View();
+        }
+
+        // POST: Flight/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Flight/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Flight/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
